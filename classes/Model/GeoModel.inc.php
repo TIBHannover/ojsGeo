e9861ffeb4a69ca3c5cac4f4b254adc56a67b820
84437883+GaziYucel@users.noreply.github.com <84437883+GaziYucel@users.noreply.github.com>
2022-06-01 12:47:39 +0200

    Main Model for Geo Plugin

---
+++ classes/Model/GeoModel.inc.php
@@ -0,0 +1,45 @@
+<?php
+namespace Optimeta\Geo\Model;
+
+class GeoModel
+{
+    /**
+     * @var string
+     * @desc The DOI for the work.
+     * @see
+     * @example https://doi.org/10.7717/peerj.4375
+     */
+    public $doi;
+
+    /**
+     * @var
+     * @desc
+     * @see
+     * @example
+     */
+    public $timestamp;
+
+    /**
+     * @var
+     * @desc
+     * @see
+     * @example
+     */
+    public $timestampDecoded;
+
+    /**
+     * @var
+     * @desc
+     * @see
+     * @example
+     */
+    public $spatialProperties;
+
+    /**
+     * @var
+     * @desc
+     * @see
+     * @example
+     */
+    public $spatialPropertiesDecoded;
+}
